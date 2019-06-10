9659ab61804f2648ff9df0df52fe36cd97c4d94c
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-09 22:51:53 -0300

    Configuração do template, estilo e lógica de apresentação do componente home.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,30 @@
+
 import { Component, OnInit } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { DenunciaModel } from '../shared/denuncia.model';
+import { DenunciaService } from '../denuncia.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [DenunciaService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  posts: DenunciaModel[];
+
+  constructor(private service: DenunciaService) { }
 
   ngOnInit() {
+    console.log('começou');
+    return this.service.getDenunciasAll()
+    .then((response: DenunciaModel[]) => this.posts = response)
+    .catch((response: any) => console.log(response));
   }
 
+  showAll(): void {
+    console.log(this.posts);
+  }
 }
