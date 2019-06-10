dd151de8e411e6d372e2dc9efe0273f3822a5dea
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-09 23:04:04 -0300

    Criação dos métodos getAll e login.

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { UserModel } from '../shared/user.model';
+import { urlApi } from '../shared/app.api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  public myUsers: UserModel[];
+
+  constructor(private serviceHttp: HttpClient) {
+    this.getAll().then((r: UserModel[]) => this.myUsers = r ).catch((r: any) => console.log(r));
+   }
+
+  public getAll(): Promise<UserModel[]> {
+    return this.serviceHttp.get(`${urlApi}/users`).toPromise()
+    .then((response: any) => response)
+    .catch((response: any) => response);
+  }
+
+  public logar(email: string, senha: string): boolean {
+    let i;
+    for(i = 0; i < this.myUsers.length; i++) {
+      if (this.myUsers[i].email == email){
+        if (this.myUsers[i].password == senha) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+    return false;
+  }
+
+  insert(newUser: UserModel): void {
+    this.myUsers.push(newUser);
+  }
+
+  private init(): void {
+  }
 }
