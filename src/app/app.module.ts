1d64fb843ce3a4d1f973894436d7f0884d4ba941
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-09 22:26:10 -0300

    Importação dos componentes recém criados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DenouceComponent } from './denouce/denouce.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewLoginComponent } from './new-login/new-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DenouceComponent,
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    MapComponent,
+    NavbarComponent,
+    NewLoginComponent
   ],
   imports: [
     BrowserModule,
