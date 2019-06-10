64f4825321f704cafd340607bf4441f5c6054c34
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-09 23:06:28 -0300

    Configuração do template, estilo e lógica de apresentação do componente new-login.

--- src/app/new-login/new-login.component.ts
+++ src/app/new-login/new-login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LoginService } from '../login/login.service';
+import { UserModel } from '../shared/user.model';
+
 @Component({
   selector: 'app-new-login',
   templateUrl: './new-login.component.html',
-  styleUrls: ['./new-login.component.css']
+  styleUrls: ['./new-login.component.css'],
+  providers: [LoginService]
 })
 export class NewLoginComponent implements OnInit {
 
-  constructor() { }
+  public newUser: UserModel;
+
+  constructor(private service: LoginService) { }
 
   ngOnInit() {
   }
 
+  private insert(user: UserModel): void {
+    this.service.insert(user);
+    alert('Inserido com sucesso --> ' + this.service.getAll());
+  }
+
+  public alerta(): void {
+    alert('Funciona');
+  }
+
+  public addUser(name: string, password: string, email: string, uf: string, city: string): void {
+    this.newUser.name = name;
+    this.newUser.email = email;
+    this.newUser.password = password;
+    this.newUser.city = city;
+    this.newUser.uf = uf;
+
+    this.insert(this.newUser);
+  }
+
 }
+
