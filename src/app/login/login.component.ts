8f29df863e53841e858cf4e6521e38baf41409a4
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-09 23:04:34 -0300

    Configuração do template, estilo e lógica de apresentação do componente login.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserModel } from '../shared/user.model';
+import { LoginService } from './login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public currentUser: UserModel;
+  private status: boolean;
+
+  constructor(private service: LoginService, private rota: Router) { }
+
+  public image: string;
 
   ngOnInit() {
+    this.image = '../../assets/login.png';
+    this.status = false;
+  }
+
+  public login(email: string, senha: string): void {
+    this.status = this.service.logar(email, senha);
+    this.status ? this.rota.navigate(['home']) : alert('Acesso negado!');
   }
 
 }
