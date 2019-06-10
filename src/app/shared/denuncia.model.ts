2ba6a9f59c863aab90c45422befb39c82454ea88
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-10 14:54:39 -0300

    Alteração do tipo de dado do atributo autor.

--- src/app/shared/denuncia.model.ts
+++ src/app/shared/denuncia.model.ts
@@ -1,10 +1,12 @@
 import { Localization } from './localization.model';
+import { UserModel } from './user.model';
+
 export class DenunciaModel {
   categoria: string;
   descricao: string;
   imagem: Object;
   id: number;
-  autor: string;
+  autor: UserModel;
   localizacao: Localization;
   status: boolean;
 }
