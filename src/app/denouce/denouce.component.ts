80914114a1a13759820917ae9c82c9c522964768
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-09 22:25:06 -0300

    Criação dos componentes iniciais da aplicação.

---
+++ src/app/denouce/denouce.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-denouce',
+  templateUrl: './denouce.component.html',
+  styleUrls: ['./denouce.component.css']
+})
+export class DenouceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
