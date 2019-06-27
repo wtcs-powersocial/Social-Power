37426d38b38284a90d8854a511d136d07012bde7
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-27 17:48:56 -0300

    Registro de novos usuários + FormsModule.

--- src/app/new-login/new-login.component.ts
+++ src/app/new-login/new-login.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 
 import { LoginService } from '../login/login.service';
 import { UserModel } from '../shared/user.model';
-import { Observable, Subscription } from 'rxjs';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-login',
@@ -14,27 +15,55 @@ import { Router } from '@angular/router';
 export class NewLoginComponent implements OnInit, OnDestroy {
 
   inscrito: Subscription;
+  img: any;
+  usuario: any;
 
   constructor(private service: LoginService, private roteador: Router) {
   }
 
   ngOnInit() {
+    this.usuario = {};
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.inscrito.unsubscribe();
+  }
+
+  criar(frm: FormGroup) {
+    console.log(this.usuario);
+    this.service.criar(this.usuario).subscribe(resposta => {
+      console.log(resposta);
+
+      frm.reset();
+    });
   }
 
   public myInsert(): void {
     // this.service.insert(this.newUser).subscribe();
   }
 
+  uploadImage(event) {
+    if (event.target.files && event.target.files[0]) {
+      this.usuario.icon = event.target.files[0];
+      // const formData = new FormData();
+      console.log(this.usuario.icon);
+    } else {
+      console.log('Nenhum arquivo foi selecionado.');
+    }
+  }
+
   public addUser(nameCompleto: string, emailUser: string, nameUser: string, pswUser: string, cpf: string, dataNasc: string): void {
     // nameCompleto.value, emailUser.value, nameUser.value, pswUser.value, emailUser.value, cpf.value, dataNasc.values
+    const formData = new FormData();
     dataNasc = '01/11/1998';
-    let newUser = new UserModel(nameCompleto, emailUser, nameUser, pswUser, cpf, dataNasc);
-    alert(newUser);
-    console.log(nameCompleto);
+    formData.append('arquivo', this.img);
+    formData.append('nameComplete', nameCompleto);
+    formData.append('email', emailUser);
+    formData.append('password', pswUser);
+    formData.append('cpf', cpf);
+    formData.append('dataNasc', dataNasc);
+    const newUser = new UserModel(nameCompleto, emailUser, nameUser, pswUser, cpf, dataNasc, this.img);
+    console.log(newUser);
     try {
     this.inscrito = this.service.insert(newUser).subscribe();
     this.roteador.navigate(['']);
