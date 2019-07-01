45266faf3bd14349d89f8d90408b8d8cf91c0789
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-07-01 15:05:28 -0300

    Front (release): autenticando novo usuário.

--- src/app/new-login/new-login.component.ts
+++ src/app/new-login/new-login.component.ts
@@ -18,7 +18,6 @@ export class NewLoginComponent implements OnInit {
   img: File;
   usuario: any;
   formData = new FormData();
-  // files: Set<File>;
 
   constructor(private service: LoginService, private roteador: Router) {
   }
@@ -27,71 +26,47 @@ export class NewLoginComponent implements OnInit {
     this.usuario = {};
   }
 
-/*
-  criar(frm: FormGroup) {
-    console.log(this.usuario);
-    this.service.insert(this.usuario).subscribe(resposta => {
-      console.log(resposta);
-
-      this.roteador.navigate(['']);
-    });
-  }
-*/
-
-  public myInsert(): void {
-    // this.service.insert(this.newUser).subscribe();
-  }
-
-  uploadImage(event) {
+  public uploadImage(event) {
     if (event.target.files && event.target.files[0]) {
       this.img = event.target.files[0];
-      // const formData = new FormData();
-      // this.files.add(this.usuario.icon);
-      console.log(this.img);
     } else {
       console.log('Nenhum arquivo foi selecionado.');
     }
   }
 
-  /*
-  private save() {
-    if (this.files && this.files.size > 0) {
-      this.service.upload(this.files).subscribe(
-        res => {
-          console.log(res);
-          this.roteador.navigate(['']);
-        },
-        err => console.log(err)
-      );
-    } else {
-      alert('Erro');
+  public addUser(): void {
+    this.populationFormData();
+    try {
+    console.log(this.formData);
+    this.service.register(this.formData).subscribe(
+      res => this.roteador.navigate(['']),
+      err => {
+        console.log(err);
+        alert(err.error.msg);
+      }
+    );
+    } catch (error) {
+      alert('Erro: não foi possível cadastrar usuário');
+      console.log(error);
     }
+    this.clearFormData();
   }
-*/
-  // addUser(nameCompleto: string, emailUser: string, pswUser: string, cpf: string, dataNasc: string)
-  public addUser(frm: FormGroup): void {
 
-    // nameCompleto.value, emailUser.value, nameUser.value, pswUser.value, emailUser.value, cpf.value, dataNasc.values
+  private populationFormData(): void {
     this.formData.append('icon', this.img, this.img.name);
     this.formData.append('nameComplete', this.usuario.nameComplete);
     this.formData.append('email', this.usuario.email);
     this.formData.append('password', this.usuario.password);
     this.formData.append('cpf', this.usuario.cpf);
     this.formData.append('dataNasc', this.usuario.dataNasc);
-    // const newUser = new UserModel(nameCompleto, emailUser, pswUser, cpf, dataNasc, this.img);
-    try {
-    console.log(this.formData);
-    this.service.register(this.formData).subscribe(
-      res => {
-        console.log(res);
-        this.roteador.navigate(['']);
-      },
-      err => alert(err)
-    );
-    } catch (error) {
-      alert(error);
-    }
-
+  }
+  private clearFormData(): void {
+    this.formData.delete('icon');
+    this.formData.delete('nameComplete');
+    this.formData.delete('email');
+    this.formData.delete('password');
+    this.formData.delete('cpf');
+    this.formData.delete('dataNasc');
   }
 
 }
