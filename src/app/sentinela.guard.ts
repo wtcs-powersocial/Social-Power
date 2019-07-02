e50a8dd14be460e3bb00999c8cbedceaa1c00f32
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-07-01 23:52:10 -0300

    Client: Criação do guarda de rotas 'sentinela'.

---
+++ src/app/sentinela.guard.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+import { LoginService } from './login/login.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SentinelaGuard implements CanActivate {
+  constructor(
+    private service: LoginService,
+    private roteador: Router) {}
+
+  canActivate(): boolean {
+    if (this.service.logged()){
+      return true;
+    } else {
+      this.roteador.navigate(['']);
+      return false;
+    }
+  }
+}
