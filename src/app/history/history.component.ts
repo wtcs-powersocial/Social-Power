7e8745e62ce07855390014be89f2eeda57214998
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-07-08 21:45:51 -0300

    Client: Ajustes nos cards.

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,6 +1,9 @@
-import { DenunciaService } from '../denuncia.service';
 import { Component, OnInit } from '@angular/core';
-import { DenunciaModel } from '../shared/denuncia.model';
+
+import { HomeService } from './../home/home.service';
+import { LoginService } from './../login/login.service';
+import { DenunciaService } from '../denuncia.service';
+
 
 @Component({
   selector: 'app-history',
@@ -9,14 +12,84 @@ import { DenunciaModel } from '../shared/denuncia.model';
 })
 export class HistoryComponent implements OnInit {
 
-  myDenouces: DenunciaModel[];
+  posts: any[];
+  user: any = null;
 
-  constructor(private service: DenunciaService) { }
+
+  constructor(private service: DenunciaService, private login: LoginService, private home: HomeService) { }
 
   ngOnInit() {
-    this.service.getDenunciasByUser(2)
-    .then((r: DenunciaModel[]) => this.myDenouces = r)
-    .catch((r: any) => console.log(r));
+    this.setUserHome();
+    this.service.getDenunciasByUser(this.getUserId()).subscribe(
+      res => {
+        this.posts = res;
+        console.log(res);
+      },
+      err => console.log(err)
+    );
+  }
+
+  private getUserId() {
+    return this.login.getUserById();
+  }
+
+  getComentario(idPost: any): any {
+    let comentario = <HTMLInputElement> document.getElementById(idPost);
+    return comentario.value;
+  }
+
+  insertLikeOrDeslike(idPost: any, content: boolean) {
+    const likeOrDeslike = {
+      userId: this.user.id,
+      userName: this.user.name,
+      like: content
+    };
+    this.home.setInteration(idPost, likeOrDeslike).subscribe(
+      res => {
+        console.log(res);
+        window.location.reload();
+      },
+      err => console.log(err)
+    );
+  }
+
+  /**
+   *
+   * @param idPost representa o identificador único da postagem referente ao comentário
+   */
+  setComentario(idPost: any) {
+    const content = this.getComentario(idPost);
+    const comment = {
+      userId: this.user.id,
+      userName: this.user.name,
+      msg: content
+    };
+    this.home.insertComment(idPost, comment).subscribe(
+      res => {
+        console.log(res);
+        window.location.reload();
+      },
+      err => console.log(err)
+    );
+  }
+
+  getLike(post: any): number {
+    return this.home.getCountLike(post);
+  }
+
+  getDeslike(post: any): number {
+    return this.home.getCountDeslike(post);
+  }
+
+  private setUserHome(): void {
+    let dados = this.login.getUser();
+    let dadoToUser = {
+      name: dados.nameComplete,
+      id:  dados._id
+    }
+    this.user = dadoToUser;
+    dados = null;
+    dadoToUser = null;
   }
 
 }
